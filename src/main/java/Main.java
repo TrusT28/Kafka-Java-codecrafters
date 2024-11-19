6c8626728dc1b2307a6548dc57cbbcd612cc11e2
Рустам Фадеев <trust.cod28@gmail.com>
2024-11-19 22:28:52 +0100

    Add ApiVersions init support, return error code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class Main {
   public static void main(String[] args){
@@ -22,6 +24,21 @@ public class Main {
       // Wait for connection from client.
       clientSocket = serverSocket.accept();
 
+      apiVersionsEndpoint(clientSocket);
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    } finally {
+      try {
+        if (clientSocket != null) {
+          clientSocket.close();
+        }
+      } catch (IOException e) {
+        System.out.println("IOException: " + e.getMessage());
+      }
+    }
+  }
+
+  public static void apiVersionsEndpoint(Socket clientSocket) throws IOException {
       // Receive data from client and parse
       InputStream inputStream = clientSocket.getInputStream();
       byte[] input_message_size = new byte[4];
@@ -36,23 +53,40 @@ public class Main {
       byte[] input_correlation_id = new byte[4];
       inputStream.read(input_correlation_id);
 
-      // Send data to client
-      OutputStream outputStream = clientSocket.getOutputStream();
-      byte[] message_size = {0,0,0,0};
-      // byte[] correlation_id = {0,0,0,7};
-      outputStream.write(message_size);
-      outputStream.write(input_correlation_id);
-
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
+      // Only support ApiVersions request
+      if (bytesToInt(input_request_api_key) == 18) {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        byte[] message_size = {0,0,0,4};
+        outputStream.write(message_size);
+        outputStream.write(input_correlation_id);
+        // Only support 0-4 versions
+        if(bytesToInt(input_request_api_version)>=0 || bytesToInt(input_request_api_version)<=4) {
+          // Send data to client
+          byte[] body = {0,0,0,0};
+          outputStream.write(body);
         }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
+        else {
+          byte[] errorCode = intToBytes(35);
+          System.out.println("Length is " + errorCode.length);
+          outputStream.write(errorCode);
+        }
       }
+  }
+
+  public static int bytesToInt(byte[] bytes) {
+    if (bytes.length == 4) {
+      return ByteBuffer.wrap(bytes).getInt();
+    }
+    else if(bytes.length == 2) {
+      return (int) ByteBuffer.wrap(bytes).getShort();
+    }
+    else {
+      throw new IllegalArgumentException("Array must contain exactly 2 or 4 bytes");
     }
   }
+
+  public static byte[] intToBytes(int value) {
+    return ByteBuffer.allocate(4).putInt(value).array();
+  }
+
 }
