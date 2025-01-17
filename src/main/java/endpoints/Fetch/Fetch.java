9c198649bf5ecbd44f74fffd5185b600620752f8
Рустам Фадеев <trust.cod28@gmail.com>
2025-01-17 19:30:16 +0100

    Fetch request add

---
+++ src/main/java/endpoints/Fetch/Fetch.java
@@ -0,0 +1,43 @@
+package endpoints.Fetch;
+
+import static utils.Utils.bytesToInt;
+import static utils.Utils.intToBytes;
+import static utils.Utils.shortToBytes;
+import utils.ErrorCodes;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import api.ApiMetadata;
+import api.RequestBody;
+import endpoints.KafkaEndpoint;
+
+public class Fetch implements KafkaEndpoint{
+
+    @Override
+    public void process(RequestBody requestBody, ByteArrayOutputStream responseBuffer) throws IOException {
+            // Only support 0-16 versions
+            if (bytesToInt(requestBody.input_request_api_version) >= 0 && bytesToInt(requestBody.input_request_api_version) <= 16) {
+                System.out.println("Handling a proper request");
+                byte[] errorCode = shortToBytes((short) ErrorCodes.NO_ERROR);
+                byte[] throttle_time_ms = intToBytes(100);
+                byte tag_buffer = 0;
+
+                // Send data to client
+                responseBuffer.write(requestBody.input_correlation_id);
+                responseBuffer.write(errorCode);
+                responseBuffer.write(throttle_time_ms);
+                // Session Id
+                responseBuffer.write(0);
+                responseBuffer.write(tag_buffer);
+            } else {
+                // Throw appropriate error code
+                System.out.println("Handling a wrong request");
+                byte[] errorCode = shortToBytes((short) ErrorCodes.WRONG_REQUEST_ERROR_CODE);
+                // specifies the size of the header and body.
+                responseBuffer.write(requestBody.input_correlation_id);
+                responseBuffer.write(errorCode);
+            }
+    }
+}
